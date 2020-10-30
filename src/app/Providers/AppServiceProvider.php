9971c8295564d06dc4a8d82d989e809578931414
kentldolar <kentldolar@gmail.com>
2020-10-31 01:16:28 +0900

    feature/ssl

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //for production
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
