4daf84543d6fcbcc9bbdf9a40439753075deb0e1
kentldolar <kentldolar@gmail.com>
2020-10-31 01:42:44 +0900

    release feature/ssl to dev

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,5 @@ yum install -y httpd
 DIR="/var/www/html/cicdaws"
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
-  rm -r /var/www/html/cicdaws
+  rm -rf /var/www/html/cicdaws
 fi
--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -3,10 +3,11 @@
 DIR="/var/www/html/cicdaws"
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
-  FILE="/var/www/html/config/.env.development"
+  FILE="/var/www/html/config/.env.production"
+  
   if [ -a "$FILE" ]; then
   # Take action if $DIR exists. #
-  cp /var/www/html/config/.env.development /var/www/html/cicdaws/.env.development
+  cp /var/www/html/config/.env.production /var/www/html/cicdaws/.env.production
   fi
 fi
 
