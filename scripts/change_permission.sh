e49b91c1962da6faec91e73839f1511e90e2befb
kentldolar <kentldolar@gmail.com>
2020-10-30 14:07:59 +0900

    release dev

--- scripts/change_permission.sh
+++ scripts/change_permission.sh
@@ -3,7 +3,7 @@
 DIR="/var/www/html/cicdaws"
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
-  sudo chown -R root:apache /var/www/html/cicdaws
+  sudo chown -R apache:apache /var/www/html/cicdaws
   sudo find /var/www/html/cicdaws -type d -exec chmod 750 {} \;
   sudo find /var/www/html/cicdaws -type f -exec chmod 640 {} \;
   sudo chmod -R 770 /var/www/html/cicdaws/storage/ /var/www/html/cicdaws/bootstrap/cache/
