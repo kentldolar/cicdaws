afe5a714f688611dc1ca9d9745f3ad28bb91ca44
kentldolar <kentldolar@gmail.com>
2020-11-05 19:07:37 +0900

    resume release 2020

--- src/resources/views/white.blade.php
+++ src/resources/views/white.blade.php
@@ -2,15 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
 
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-M3XNJTQ');</script>
-        <!-- End Google Tag Manager -->
-
-
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">        
@@ -38,11 +29,6 @@
         <link href="{{ asset('public/white/theme/css/style.css') }}" rel="stylesheet">
     </head>
     <body  data-spy="scroll" data-target=".menu-area" data-offset="200">
-    
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3XNJTQ"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
 
     <!--wrapper page-->
     <div class="wrapper">
